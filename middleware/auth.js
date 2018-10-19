37d1525270aaf1f8f45b2211b49ad43778ab46a6
Saransh Pal <palsaransh99@gmail.com>
2018-10-19 11:22:12 +0530

    some minor tweaks

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,15 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-module.exports = function(req, res, next){
+module.exports = function(req, res, next) {
     const token = req.header('x-auth-token');
-    if(!token) return res.status(401).send('Access Denied No token BITCH!!');
+    if (!token) return res.status(401).send('Access Denied No token BITCH!!');
 
-    try{
+    try {
         const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
         req.user = decoded;
         next();
-
-    }catch(ex){
+    } catch (ex) {
         res.status(400).send('Invalid TOKEN BITCH!!!!');
     }
-
-}
+};
--- models/userModel.js
+++ models/userModel.js
@@ -3,20 +3,18 @@ const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-
 const userSchema = new mongoose.Schema({
-
     firstName: {
         type: String,
         required: true,
-        minlength: 5,
+        minlength: 4,
         maxlength: 50
     },
 
     lastName: {
         type: String,
         required: true,
-        minlength: 5,
+        minlength: 3,
         maxlength: 50
     },
 
@@ -42,19 +40,34 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign({_id: this._id, isAdmin: this.isAdmin}, config.get('jwtPrivateKey'));
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign(
+        { _id: this._id, isAdmin: this.isAdmin },
+        config.get('jwtPrivateKey')
+    );
     return token;
-}
+};
 
 const User = mongoose.model('User', userSchema);
 
-function validateUser(user){
+function validateUser(user) {
     const schema = {
-        firstName: Joi.string().min(5).max(50).required(),
-        lastName: Joi.string().min(5).max(50).required(),
-        email: Joi.string().min(5).max(255).required(),
-        password: Joi.string().min(5).max(255).required(),
+        firstName: Joi.string()
+            .min(4)
+            .max(50)
+            .required(),
+        lastName: Joi.string()
+            .min(3)
+            .max(50)
+            .required(),
+        email: Joi.string()
+            .min(5)
+            .max(255)
+            .required(),
+        password: Joi.string()
+            .min(5)
+            .max(255)
+            .required(),
         isAdmin: Joi.boolean().required()
     };
     return Joi.validate(user, schema);
