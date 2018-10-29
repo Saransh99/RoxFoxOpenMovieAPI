37ab486eeb21465ee921f252b22677af5d588ca3
Saransh Pal <palsaransh99@gmail.com>
2018-10-29 21:04:46 +0530

    added some more end points

--- startup/routes.js
+++ startup/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const genres = require('../routes/genres');
 const movies = require('../routes/movies');
 const tvgenres = require('../routes/tvgenres');
@@ -8,9 +10,13 @@ const anime = require('../routes/anime');
 const games = require('../routes/games');
 const auth = require('../routes/auth');
 const home = require('../routes/home');
+const celebs = require('../routes/celebs');
+const musicAlbum = require('../routes/musicAlbum');
 const error = require('../middleware/error');
 
-module.exports = function(app) {
+module.exports = function (app) {
+    //! This is the 3rd party middleware to enable the cors in the server side 
+    app.use(cors());
     app.use(express.json());
     app.use('/api/genres', genres);
     app.use('/api/movies', movies);
@@ -20,6 +26,14 @@ module.exports = function(app) {
     app.use('/api/anime', anime);
     app.use('/api/games', games);
     app.use('/api/auth', auth);
+    app.use('/api/celebs', celebs);
+    app.use('/api/musicAlbum', musicAlbum);
     app.use('/', home);
+
+    // *to limit the request of the api 
+    app.use(rateLimit({
+        windowMs: 30 * 1000, // 30s
+        max: 10
+    }));
     app.use(error);
 };
